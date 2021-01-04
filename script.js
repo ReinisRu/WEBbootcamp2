d499e3cea816c1afea252bbc24f635af86fca898
ReinisRu <rr@itfly.lv>
2021-01-04 19:10:09 +0200

    cards 1. lekcijas stadija

---
+++ script.js
@@ -0,0 +1,31 @@
+let cards = document.querySelectorAll(".container div");
+
+let last_card = false;
+let game_on = true;
+for (let card of cards) {
+  card.addEventListener("click", function () {
+    if (game_on === true) {
+      if (
+        last_card !== false &&
+        last_card !== card &&
+        last_card.textContent === card.textContent
+      ) {
+        card.style.background = "lightgreen";
+        last_card.style.background = "lightgreen";
+        addRestartButton();
+        game_on = false;
+      }
+      last_card = card;
+    }
+  });
+}
+
+function addRestartButton() {
+  let button = document.createElement("button");
+  button.textContent = "Reset";
+  button.addEventListener("click", function () {
+    location.reload();
+  });
+
+  document.querySelector("body").append(button);
+}
